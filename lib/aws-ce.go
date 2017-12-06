c5b06419e2e40a17591716332f3611a3ed8d6f6e
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-12-06 10:30:57 +0000

    Get costs by linked account

--- lib/aws-ce.go
+++ lib/aws-ce.go
@@ -2,7 +2,13 @@ package mpawsce
 
 import (
 	"flag"
+	"fmt"
+	"log"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/costexplorer"
 	mp "github.com/mackerelio/go-mackerel-plugin"
 )
 
@@ -27,12 +33,56 @@ type CEPlugin struct {
 	AccessKeyID     string
 	SecretAccessKey string
 	Region          string
+	CostExplorer    *costexplorer.CostExplorer
+}
+
+func (c CEPlugin) createConnection() (*costexplorer.CostExplorer, error) {
+	var creds = credentials.NewSharedCredentials("", "default")
+	conn := costexplorer.New(session.New(&aws.Config{Credentials: creds, Region: &c.Region}))
+	return conn, nil
 }
 
 // FetchMetrics interface for mackerelplugin
 func (c CEPlugin) FetchMetrics() (map[string]float64, error) {
 	ret := make(map[string]float64)
 
+	fmt.Println(c.CostExplorer.GetCostAndUsage(&costexplorer.GetCostAndUsageInput{
+		Granularity: aws.String("MONTHLY"),
+		TimePeriod: &costexplorer.DateInterval{
+			Start: aws.String("2017-11-01"),
+			End:   aws.String("2017-12-01"),
+		},
+		Metrics: []*string{
+			aws.String("UnblendedCost"),
+		},
+		GroupBy: []*costexplorer.GroupDefinition{
+			&costexplorer.GroupDefinition{
+				Type: aws.String("DIMENSION"),
+				Key:  aws.String("LINKED_ACCOUNT"),
+			},
+		},
+	}))
+
+	// gd1 := costexplorer.GroupDefinition{
+	// 	Type: aws.String("DIMENSION"),
+	// 	Key:  aws.String("LINKED_ACCOUNT"),
+	// }
+
+	// gb := []*costexplorer.GroupDefinition{}
+	// gb = append(gb, &gd1)
+
+	// input := costexplorer.GetCostAndUsageInput{
+	// 	GroupBy: gb,
+	// }
+	// fmt.Println(c.CostExplorer)
+	// input := costexplorer.GetCostAndUsageInput{}
+
+	// result, err := c.CostExplorer.GetCostAndUsage(&input)
+	// if err != nil {
+	// 	return ret, err
+	// }
+
+	// fmt.Println(result)
 	ret["billing.hogehoge.total"] = 1.0
 	return ret, nil
 
@@ -63,9 +113,17 @@ func Do() {
 	flag.Parse()
 
 	var ce CEPlugin
+
 	ce.Prefix = *optPrefix
 	ce.AccessKeyID = *optAccessKeyID
 	ce.SecretAccessKey = *optSecretAccessKey
+	ce.Region = region
+
+	var err error
+	ce.CostExplorer, err = ce.createConnection()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	helper := mp.NewMackerelPlugin(ce)
 	helper.Tempfile = *optTempfile
