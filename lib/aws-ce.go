dbb062e6f2bca921161e2592ef1760a0da2469a3
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-12-07 10:12:21 +0000

    Add forecast

--- lib/aws-ce.go
+++ lib/aws-ce.go
@@ -21,13 +21,22 @@ const (
 )
 
 var graphdef = map[string]mp.Graphs{
-	"billing.#": {
+	"usage.#": {
 		Label: "AWS Monthly Billing",
 		Unit:  "integer",
 		Metrics: []mp.Metrics{
 			{Name: "BlendedCost", Label: "Blended Cost", Diff: false, Stacked: true},
 			{Name: "UnblendedCost", Label: "Unblended Cost", Diff: false, Stacked: true},
 			{Name: "UsageQuantity", Label: "Usage Quantity", Diff: false, Stacked: true},
+		},
+	},
+	"forecast.#": {
+		Label: "AWS Monthly Billing Forecast",
+		Unit:  "integer",
+		Metrics: []mp.Metrics{
+			{Name: "ForecastBlendedCost", Label: "Forecast Blended Cost", Diff: false, Stacked: true},
+			{Name: "ForecastUnblendedCost", Label: "Forecast Unblended Cost", Diff: false, Stacked: true},
+			{Name: "ForecastUsageQuantity", Label: "Forecast Usage Quantity", Diff: false, Stacked: true},
 		},
 	},
 }
@@ -37,6 +46,7 @@ type CEPlugin struct {
 	Prefix          string
 	Metrics         string
 	DisableName     bool
+	EnableForecast  bool
 	AccessKeyID     string
 	SecretAccessKey string
 	Region          string
@@ -55,8 +65,15 @@ func (c CEPlugin) FetchMetrics() (map[string]float64, error) {
 	ret := make(map[string]float64)
 
 	now := time.Now().UTC()
-	start := fmt.Sprintf("%d-%d-01", now.Year(), now.Month())
+	firstday := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
+	lastday := firstday.AddDate(0, 1, 0)
+
+	start := fmt.Sprintf("%d-%d-%02d", firstday.Year(), firstday.Month(), firstday.Day())
 	end := fmt.Sprintf("%d-%d-%02d", now.Year(), now.Month(), now.Day())
+	last := fmt.Sprintf("%d-%d-%02d", lastday.Year(), lastday.Month(), lastday.Day())
+
+	elasp := float64(now.Unix() - firstday.Unix())
+	period := float64(lastday.Unix() - firstday.Unix())
 
 	dimentionValues, err := c.CostExplorer.GetDimensionValues(&costexplorer.GetDimensionValuesInput{
 		Dimension: aws.String("LINKED_ACCOUNT"),
@@ -110,9 +127,15 @@ func (c CEPlugin) FetchMetrics() (map[string]float64, error) {
 			key = accounts[*g.Keys[0]]
 		}
 
-		ret["billing."+key+"."+c.Metrics], err = strconv.ParseFloat(*g.Metrics[c.Metrics].Amount, 64)
+		usage, err := strconv.ParseFloat(*g.Metrics[c.Metrics].Amount, 64)
 		if err != nil {
 			return ret, err
+		}
+
+		ret["usage."+key+"."+c.Metrics] = usage
+
+		if c.EnableForecast {
+			ret["forecast."+key+"."+"Forecast"+c.Metrics] = usage * period / elasp
 		}
 
 	}
@@ -141,6 +164,7 @@ func Do() {
 		optPrefix          = flag.String("metric-key-prefix", "aws-ce", "Metric key prefix.")
 		optMetrics         = flag.String("metrics", "UnblendedCost", "Choise from [BlendedCost, UnblendedCost, UsageQuantity].")
 		optDisableName     = flag.Bool("disable-name", false, "Disable to get account name. Output account ID.")
+		optEnableForecast  = flag.Bool("enable-forecast", false, "Enable to forecast billing.")
 		optAccessKeyID     = flag.String("access-key-id", "", "AWS Access Key ID")
 		optSecretAccessKey = flag.String("secret-access-key", "", "AWS Secret Access Key")
 		optTempfile        = flag.String("tempfile", "", "Temp file name")
@@ -152,6 +176,7 @@ func Do() {
 	ce.Prefix = *optPrefix
 	ce.Metrics = *optMetrics
 	ce.DisableName = *optDisableName
+	ce.EnableForecast = *optEnableForecast
 	ce.AccessKeyID = *optAccessKeyID
 	ce.SecretAccessKey = *optSecretAccessKey
 	ce.Region = region
